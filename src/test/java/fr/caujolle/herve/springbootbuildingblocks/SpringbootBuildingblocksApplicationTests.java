7f7654c594ffd20eacf4956aa8bb28b5a6dfb65d
hcau <herve.caujolle@laposte.net>
2022-04-11 12:11:11 +0200

    Initialisation.

---
+++ src/test/java/fr/caujolle/herve/springbootbuildingblocks/SpringbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.caujolle.herve.springbootbuildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
